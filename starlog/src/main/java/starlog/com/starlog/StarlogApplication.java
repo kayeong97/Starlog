ce842dfa1b2e2ee0dc02152e966b1400e93b1173
kayeong <kimkayeong182@naver.com>
2026-04-28 20:39:33 +0900

    spring boot 기본 세팅

---
+++ starlog/src/main/java/starlog/com/starlog/StarlogApplication.java
@@ -0,0 +1,13 @@
+package starlog.com.starlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarlogApplication.class, args);
+	}
+
+}
